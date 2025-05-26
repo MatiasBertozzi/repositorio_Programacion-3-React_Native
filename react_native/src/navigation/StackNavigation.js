de9a5f367e70591ddd2d13ae1be5afd470da954b
FacundoStorani <storanif@udesa.edu.ar>
2025-05-26 11:43:46 -0300

    Para pull

--- react_native/src/navigation/StackNavigation.js
+++ react_native/src/navigation/StackNavigation.js
@@ -10,42 +10,23 @@ export default function StackNavigation() {
     return(
     <Stack.Navigator>
         <Stack.Screen
-<<<<<<< HEAD
-
-            name='Registro' 
-=======
         name='Registro' 
->>>>>>> f69c5d179cb8e0e1b6acecc5964c8b099e8f1f11
             component={Registro}
             options={
                 {headerShown: false}}/>
 
          <Stack.Screen
-<<<<<<< HEAD
-
-=======
->>>>>>> f69c5d179cb8e0e1b6acecc5964c8b099e8f1f11
             name='Login' 
             component={Login}
             options={
                 {headerShown: false}}/>
 
         <Stack.Screen
-<<<<<<< HEAD
-            name='Tab'
-            component={BottomTabs}
-            options={{
-                headerShown:false
-            }}
-        />
-
-=======
          name='Tab'
         component={BottomTabs}
         options={{
             headerShown:false
             }}/>
->>>>>>> f69c5d179cb8e0e1b6acecc5964c8b099e8f1f11
 
     </Stack.Navigator>
 
