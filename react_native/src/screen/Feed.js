b44bfd2f5f0acaf0ae57e6d252ffda6b5a1047e0
FacundoStorani <storanif@udesa.edu.ar>
2025-05-26 13:14:49 -0300

    Feed terminado, falta ver como funciona duando se cran posts, por ahora solo funciona el spiner

--- react_native/src/screen/Feed.js
+++ react_native/src/screen/Feed.js
@@ -1,12 +1,69 @@
-import { Text, View } from 'react-native'
+import { Text, View, ActivityIndicator } from 'react-native'
 import React, { Component } from 'react'
 
 export default class Feed extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Feed</Text>
-      </View>
-    )
-  }
+  constructor(props) {
+  super(props);
+  this.state = {
+    posts: [],
+    loading: true
+  };
+}
+
+ render() {
+  return (
+    <View>
+      {this.state.loading ? (
+        <View>
+          <ActivityIndicator />
+          <Text>Cargando posteos...</Text>
+        </View>
+      ) : this.state.posts.length === 0 ? (
+        <View>
+          <Text>No hay posteos todavía.</Text>
+        </View>
+      ) : (
+        <FlatList
+          data={this.state.posts}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View>
+              <Text>Autor: {item.data.email}</Text>
+              <Text>Post: {item.data.contenido}</Text>
+              <Text>Likes: {item.data.likes.length}</Text>
+
+              <TouchableOpacity
+                onPress={() => {
+                  let email = auth.currentUser.email;
+
+                  if (item.data.likes.includes(email)) {
+                    db.collection('posts')
+                      .doc(item.id)
+                      .update({
+                        likes: firebase.firestore.FieldValue.arrayRemove(email)
+                      });
+                  } else {
+                    db.collection('posts')
+                      .doc(item.id)
+                      .update({
+                        likes: firebase.firestore.FieldValue.arrayUnion(email)
+                      });
+                  }
+                }}
+              >
+                <Text>
+                  {item.data.likes.includes(auth.currentUser.email)
+                    ? 'Quitar Like'
+                    : 'Dar Like'}
+                </Text>
+              </TouchableOpacity>
+
+            </View>
+          )}
+        />
+      )}
+    </View>
+  );
+}
+
 }
